7b380f13fb3d83d2fb660afcb8ab8624816ece28
Max <41771785+maxh1231@users.noreply.github.com>
2025-04-02 21:07:21 -0600

    git log fzf w preview

--- aliases.zsh
+++ aliases.zsh
@@ -2,7 +2,9 @@
 
 alias mongostart="brew services start mongodb-community";
 alias mongostop="brew services stop mongodb-community";
-alias glols="git log --pretty=format:"%h%x09%an%x09%ad%x09%s""
+alias glols="git log --pretty=format:"%h%x09%an%x09%ad%x09%s"";
+alias glol='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset"'
 alias glb="git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | \
   grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[\$1]++' | head -n 10 | \
-  awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'"
+  awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'";
+alias filediff="git log --no-merges --color --format=' * %C(red)%h%C(reset) - %s %C(green)(%ar)%C(reset) %C(bold brightblue)<%an>%C(reset)' | fzf --ansi --print-query --preview 'git diff-tree --no-commit-id --name-only -r $(echo {2})'"
