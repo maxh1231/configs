b0aa78afd10e39438b9900866ea4ed06d0e57cf8
Max <41771785+maxh1231@users.noreply.github.com>
2025-04-05 00:40:56 -0600

    git branch fzf git switch

--- aliases.zsh
+++ aliases.zsh
@@ -6,4 +6,5 @@ alias glol='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgre
 alias glb="git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | \
   grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[\$1]++' | head -n 10 | \
   awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr(\$2, 1, length(\$2)-1), \$1)}'"
-alias gld='git log --no-merges --color --format=" * %C(red)%h%C(reset) - %s %C(green)(%ar)%C(reset) %C(bold brightblue)<%an>%C(reset)" | fzf --ansi --preview "git diff-tree --no-commit-id --name-only -r {2}" | awk "{print \$2}" | xargs git diff'
+alias gld='git status && git log --no-merges --color --format=" * %C(red)%h%C(reset) - %s %C(green)(%ar)%C(reset) %C(bold brightblue)<%an>%C(reset)" | fzf --ansi --layout="reverse" --preview "git diff-tree --no-commit-id --name-only -r {2}" | awk "{print \$2}" | xargs -I {} git diff {}^ {}'
+alias gbs='cb=$(git branch --show-current) && { echo "$cb"; git branch --format="%(refname:short)" --sort=-committerdate | grep -v "$cb"; } | fzf --layout="reverse" | xargs -I {} git switch {}'
